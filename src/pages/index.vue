084df201f53d9ffc3d489531b6da00ee228b5c85
liyin <shuaiyin.top@foxmail.com>
2022-05-08 20:07:47 +0800

    feat: 点击展开周围没有炸弹的元素

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -2,7 +2,7 @@
 interface BlockState {
   x: number
   y: number
-  revealed?: boolean
+  revealed: boolean
   mine?: boolean
   flagged?: boolean
   adjacentMines: number
@@ -26,16 +26,29 @@ const state = reactive(
   Array.from({ length: HEIGHT },
     (_, y) => Array.from(
       { length: WIDTH },
-      (_, x): BlockState => ({ x, y, adjacentMines: 0 }),
+      (_, x): BlockState => ({
+        x,
+        y,
+        adjacentMines: 0,
+        revealed: false,
+      }),
     ),
   ),
 )
 
-function generateMines() {
+function generateMines(initial: BlockState) {
   for (const row of state) {
-    for (const block of row)
+    for (const block of row) {
+      if (Math.abs(initial.x - block.x) <= 1)
+        continue
+      if (Math.abs(initial.y - block.y) <= 1)
+        continue
+
       block.mine = Math.random() < 0.3
+    }
   }
+
+  updateNumbers()
 }
 
 // 计算旁边有多少炸弹💣
@@ -44,18 +57,27 @@ function updateNumbers() {
     raw.forEach((block, x) => {
       if (block.mine)
         return
-      directions.forEach(([dx, dy]) => {
-        const x2 = x + dx
-        const y2 = y + dy
-        // 判断是否出界了
-        if (x2 < 0 || x2 >= WIDTH || y2 < 0 || y2 >= HEIGHT)
-          return
-
-        if (state[y2][x2].mine)
-          block.adjacentMines += 1
+      getSiblings(block).forEach((sibling: BlockState) => {
+        if (sibling.mine)
+          block.adjacentMines++
       })
     })
   })
+}
+
+// 获得某个元素相邻的点
+function getSiblings(block: BlockState): BlockState[] {
+  return directions
+    .map(([dx, dy]) => {
+      const x2 = block.x + dx
+      const y2 = block.y + dy
+      // 判断是否出界了
+      if (x2 < 0 || x2 >= WIDTH || y2 < 0 || y2 >= HEIGHT)
+        return undefined
+
+      return state[y2][x2]
+    })
+    .filter(Boolean) as BlockState[]
 }
 
 const numberColors = [
@@ -70,17 +92,39 @@ const numberColors = [
 ]
 
 function getBlockClass(block: BlockState) {
+  if (!block.revealed)
+    return 'bg-gray-500/10'
+
   return block.mine
-    ? 'bg-red'
+    ? 'bg-red-500/50'
     : numberColors[block.adjacentMines]
 }
 
-generateMines()
-updateNumbers()
+let mineGenerated = false
+const dev = true
+function onClick(block: BlockState) {
+  if (!mineGenerated) {
+    generateMines(block)
+    mineGenerated = true
+  }
 
-function onClick(x, y) {
-  alert('Hello World!')
+  block.revealed = true
+  expandZero(block)
 }
+
+function expandZero(block: BlockState) {
+  if (block.adjacentMines > 0)
+    return
+
+  getSiblings(block)
+    .forEach((sibling) => {
+      if (!sibling.revealed) {
+        sibling.revealed = true
+        expandZero(sibling)
+      }
+    })
+}
+
 </script>
 
 <template>
@@ -93,19 +137,21 @@ function onClick(x, y) {
         flex items-center justify-center
       >
         <button
-          v-for="item, x in raw"
+          v-for="block, x in raw"
           :key="x"
           w-10 h-10 m="0.5"
           flex items-center justify-center
           hover="bg-gray/10"
           border="1 gray-400/10"
-          :class="getBlockClass(item)"
-          @click="onClick(x, y )"
+          :class="getBlockClass(block)"
+          @click="onClick(block)"
         >
-          <div v-if="item.mine" i-mdi-mine />
-          <div v-else>
-            {{ item.adjacentMines }}
-          </div>
+          <template v-if="block.revealed || dev">
+            <div v-if="block.mine" i-mdi-mine />
+            <div v-else>
+              {{ block.adjacentMines }}
+            </div>
+          </template>
         </button>
       </div>
     </div>
