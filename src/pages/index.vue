648875ff737b7e1b83d009042f559f23ce2d03e7
liyin <shuaiyin.top@foxmail.com>
2022-05-09 22:16:53 +0800

    feat: 将 BlockState 类型移出去

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,12 +1,5 @@
 <script lang="ts" setup>
-interface BlockState {
-  x: number
-  y: number
-  revealed: boolean
-  mine?: boolean
-  flagged?: boolean
-  adjacentMines: number
-}
+import type { BlockState } from '~/types'
 
 const WIDTH = 5
 const HEIGHT = 5
